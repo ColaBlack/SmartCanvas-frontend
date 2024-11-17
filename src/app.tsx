159235aa29e387c22cf34718f4011d846afd8be0
ColaBlack <2698360477@qq.com>
2024-11-17 15:01:24 +0800

    update base请求地址

--- src/app.tsx
+++ src/app.tsx
@@ -133,6 +133,6 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
  * @doc https://umijs.org/docs/max/request#配置
  */
 export const request = {
-  baseUrl: 'http://localhost:1221',
+  baseURL: 'http://localhost:1221',
   ...errorConfig,
 };
