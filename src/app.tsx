4de41acf39df7217b263a2103a84ac1b24b78ec3
ColaBlack <2698360477@qq.com>
2024-11-17 14:06:04 +0800

    修改了主题色

--- src/app.tsx
+++ src/app.tsx
@@ -110,7 +110,7 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
             <SettingDrawer
               disableUrlParams
               enableDarkTheme
-              settings={initialState?.settings}
+              settings={defaultSettings}
               onSettingChange={(settings) => {
                 setInitialState((preInitialState) => ({
                   ...preInitialState,
