7d3da376534a24a78fef2d5773d8fcb79883f0b4
ColaBlack <2698360477@qq.com>
2024-11-23 15:20:29 +0800

    remove 无用代码

--- src/components/RightContent/AvatarDropdown.tsx
+++ src/components/RightContent/AvatarDropdown.tsx
@@ -1,13 +1,12 @@
-import { outLogin } from '@/services/ant-design-pro/api';
-import { LogoutOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
+import { LogoutOutlined } from '@ant-design/icons';
 import { history, useModel } from '@umijs/max';
-import { Spin } from 'antd';
+import { message, Spin } from 'antd';
 import { createStyles } from 'antd-style';
-import { stringify } from 'querystring';
 import type { MenuInfo } from 'rc-menu/lib/interface';
 import React, { useCallback } from 'react';
 import { flushSync } from 'react-dom';
 import HeaderDropdown from '../HeaderDropdown';
+import { userLogoutUsingPost } from '@/services/SmartCanvas/userController';
 
 export type GlobalHeaderRightProps = {
   menu?: boolean;
@@ -40,24 +39,14 @@ const useStyles = createStyles(({ token }) => {
 
 export const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({ menu, children }) => {
   /**
-   * 退出登录，并且将当前的 url 保存
+   * 退出登录
    */
   const loginOut = async () => {
-    await outLogin();
-    const { search, pathname } = window.location;
-    const urlParams = new URL(window.location.href).searchParams;
-    /** 此方法会跳转到 redirect 参数所在的位置 */
-    const redirect = urlParams.get('redirect');
-    // Note: There may be security issues, please note
-    if (window.location.pathname !== '/user/login' && !redirect) {
-      history.replace({
-        pathname: '/user/login',
-        search: stringify({
-          redirect: pathname + search,
-        }),
-      });
-    }
+    await userLogoutUsingPost();
+    message.success('退出登录成功');
+    history.replace({ pathname: '/user/login' });
   };
+
   const { styles } = useStyles();
 
   const { initialState, setInitialState } = useModel('@@initialState');
@@ -72,7 +61,6 @@ export const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({ menu, childre
         loginOut();
         return;
       }
-      history.push(`/account/${key}`);
     },
     [setInitialState],
   );
@@ -99,37 +87,18 @@ export const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({ menu, childre
     return loading;
   }
 
-  const menuItems = [
-    ...(menu
-      ? [
-          {
-            key: 'center',
-            icon: <UserOutlined />,
-            label: '个人中心',
-          },
-          {
-            key: 'settings',
-            icon: <SettingOutlined />,
-            label: '个人设置',
-          },
-          {
-            type: 'divider' as const,
-          },
-        ]
-      : []),
-    {
-      key: 'logout',
-      icon: <LogoutOutlined />,
-      label: '退出登录',
-    },
-  ];
-
   return (
     <HeaderDropdown
       menu={{
         selectedKeys: [],
         onClick: onMenuClick,
-        items: menuItems,
+        items: [
+          {
+            key: 'logout',
+            icon: <LogoutOutlined />,
+            label: '退出登录',
+          },
+        ],
       }}
     >
       {children}
